b7b2d66a64c47419bc31803b9b764410ded90991
Alejandro Garcia <alejandrojg8@gmail.com>
2023-02-28 21:42:18 +0000

    Add communication between Orders and Inventory to check if product is in stock

--- inventory-service/src/main/java/com/programmingPractise/inventoryservice/controller/InventoryController.java
+++ inventory-service/src/main/java/com/programmingPractise/inventoryservice/controller/InventoryController.java
@@ -1,9 +1,12 @@
 package com.programmingPractise.inventoryservice.controller;
 
+import com.programmingPractise.inventoryservice.dto.InventoryResponse;
 import com.programmingPractise.inventoryservice.service.InventoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/inventory")
@@ -12,9 +15,15 @@ public class InventoryController {
 
     private final InventoryService inventoryService;
 
-    @GetMapping("/{sku-code}")
+    //http://localhost:8082/inventory/iphone-13,iphone13-red
+    //
+    // http://localhost:8082/inventory?skuCode=iphone-13&skuCode=iphone13-red
+
+
+
+    @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public boolean isInStock(@PathVariable("sku-code") String skuCode){
+    public List<InventoryResponse> isInStock(@RequestParam List<String> skuCode){
         return inventoryService.isInStock(skuCode);
     }
 }
